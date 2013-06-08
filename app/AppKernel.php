b00083fe8cff5e5246ce6ee1c6d3e9df29678410
Justen Doherty <phpchap@gmail.com>
2013-06-08 16:26:25 +0100

    Added Bootstrap and initial theme

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,8 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Atlas\BookingBundle\AtlasBookingBundle(),
-            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle()            
+            new Bc\Bundle\BootstrapBundle\BcBootstrapBundle()
+            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
